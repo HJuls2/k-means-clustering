e8199ce64393d764ca3bfd789d196c393538ac4a
HJuls2 <giuliobaronti@gmail.com>
2021-12-27 19:03:21 +0100

    improved generation and reading for clusters

--- generator.py
+++ generator.py
@@ -1,27 +1,30 @@
 import sys
 import csv
 import random as rand
+from numpy import float64
+from sklearn.datasets import make_blobs
+import numpy as np
+import pandas as pd
 
 
-def generate(num,lowbound,upbound):
-    return [(rand.randint(lowbound,upbound),rand.randint(lowbound,upbound)) for n in range(num)]
+def generateClusters(samples, centers, devStd, lowBound, upBound):
+    return make_blobs(n_samples=samples,n_features=2,centers=centers, cluster_std=devStd, center_box=(lowBound,upBound), shuffle=True)
+
+
+def writeOnFile(points, filename):
+    pd.DataFrame(points).to_csv(path_or_buf=filename+'.csv', header=['x','y'])
 
  
 if __name__ == '__main__':
-    filename=sys.argv[1]
-    args=[int(arg) for arg in sys.argv[2:]]
-    dataset=generate(args[0], args[1],args[2])
+    filename = sys.argv[1]
+    samples = int(sys.argv[2])
+    clusters = int(sys.argv[3])
+    devStd = float64(sys.argv[4])
+    lowerBound = float64(sys.argv[5])
+    upperBound = float64(sys.argv[6])
+
+    points,_ = generateClusters(samples,clusters,devStd, lowBound=lowerBound,upBound=upperBound)
+    writeOnFile(points,filename)
     
-    file=open(filename+".csv", 'w')
     
-    with file:
-        fields=['x','y']
-        writer=csv.DictWriter(file, fields)
-        writer.writeheader()
-        
-        
-        for pair in dataset:
-            x=pair[0]
-            y=pair[1]
-            writer.writerow({'x': x,'y':y})
     
--- plotter.py
+++ plotter.py
@@ -1,24 +1,12 @@
 import matplotlib.pyplot as plt
 import sys
 import csv
+import pandas as pd
 
 if __name__ == '__main__':
     filename=sys.argv[1]
-    
-    
-    with open(filename, mode="r") as file:
-        reader=csv.reader(file)
-        
-        for pair in reader:
-            try:
-                plt.plot(int(pair[0]),int(pair[1]),'bo')
-                print
-            except ValueError:
-                print('The first line is ignored')
-                     
-            
-    
-    plt.xlabel('x')
-    plt.ylabel('y')
+
+    dataframe = pd.read_csv(filename)
+    dataframe.plot(kind='scatter',x='x', y='y')
     plt.show()
     
